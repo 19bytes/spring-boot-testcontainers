d48b11faa141da500d4e868ae713fd06ebac467f
Joscha Burkholz <joscha.burkholz@19bytes.de>
2020-05-15 20:53:34 +0200

    intial commit

---
+++ src/test/java/de/nineteenbytes/springboottestcontainers/SpringBootTestcontainersApplicationTests.java
@@ -0,0 +1,13 @@
+package de.nineteenbytes.springboottestcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestcontainersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
